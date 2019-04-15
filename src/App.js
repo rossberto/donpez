72c98f530f7c19a8e17593905d355a16deea01dc
ross <dev@rross.me>
2019-04-15 13:26:50 -0500

    First commit: Sale point basic layout created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './donpez.png';
 import './App.css';
 
 class App extends Component {
   render() {
+    let fecha = new Date();
+    let hora = fecha.getHours();
+    let minutos = fecha.getMinutes();
+    if (minutos<10) {
+      minutos = '0' + minutos;
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div class="flex-container">
+        <div class="main-pad">
+          <div class="item-pad">
+            <h2>Pescado</h2>
+            <input class="cantidad" placeholder="Cantidad" value="2"></input>
+            <button class="remove-taco" type="button" name="remove">-</button>
+            <button class="add-taco" type="button" name="add">+</button>
+          </div>
+          <div class="item-pad">
+            <h2>Camaron</h2>
+            <input class="cantidad" placeholder="Cantidad" value="2"></input>
+            <button class="remove-taco" type="button" name="remove">-</button>
+            <button class="add-taco" type="button" name="add">+</button>
+          </div>
+          <div class="item-pad">
+            <h2>Refrescos</h2>
+            <input class="cantidad" placeholder="Cantidad" value="2"></input>
+            <button class="remove-taco" type="button" name="remove">-</button>
+            <button class="add-taco" type="button" name="add">+</button>
+          </div>
+        </div>
+        <div class="main-pad">
+          <div class="total">
+            <h2>TOTAL A PAGAR</h2>
+            <h1>$120</h1>
+            <h2>Entrega</h2>
+            <input class="entrega" placeholder="$0"></input>
+            <h2>Cambio</h2>
+            <h1>$80</h1>
+            <button class="total-button">Borrar</button>
+            <button class="total-button">Cobrar</button>
+          </div>
+        </div>
+      </div>
       </div>
     );
   }
